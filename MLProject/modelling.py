11f46280af39e8628c20aa4971b92441a126d122
meidhika <meidhikanawa@gmail.com>
2026-06-02 17:00:08 +0700

    fix modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,9 +4,6 @@ import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Autolog untuk mencatat metrik dan artefak model secara lokal di runner CI
-mlflow.sklearn.autolog()
-
 def run_training():
     print("Memuat data...")
     train_df = pd.read_csv("credit_scoring_preprocessing/train.csv")
@@ -24,6 +21,24 @@ def run_training():
         preds = model.predict(X_test)
         acc = accuracy_score(y_test, preds)
         print(f"Akurasi: {acc}")
+        
+        # MEMAKSA MLFLOW MENGGUNAKAN PYTHON 3.12.7 UNTUK DOCKER
+        custom_env = {
+            "name": "credit-scoring-env",
+            "channels": ["conda-forge"],
+            "dependencies": [
+                "python=3.12.7",  # Kunci utamanya di sini
+                "pip",
+                {"pip": ["pandas", "scikit-learn", "mlflow==2.19.0"]}
+            ]
+        }
+        
+        # Menyimpan model beserta environment yang sudah ditentukan
+        mlflow.sklearn.log_model(
+            sk_model=model, 
+            artifact_path="model", 
+            conda_env=custom_env
+        )
 
 if __name__ == "__main__":
     run_training()
